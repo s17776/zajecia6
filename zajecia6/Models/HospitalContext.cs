9933f731ae0f8f6688fbd6a0fd3826f014bf8d64
Marek Palasek <s17776@pjwstk.edu.pl>
2020-06-07 15:58:20 +0200

    Dodaj pliki projektów.

---
+++ zajecia6/Models/HospitalContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zajecia6.Models
+{
+    public class HospitalContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public HospitalContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Doctor>().HasData(new Doctor
+            {
+                IdDoctor = 1,
+                FirstName = "Ferdek",
+                LastName = "Kiepski",
+                Email = "feredekKiepski@gmail.com"
+            });
+            modelBuilder.Entity<Patient>().HasData(new Patient
+            {
+                IdPatient = 1,
+                FirstName = "Marian",
+                LastName = "Pazdzioch",
+                BirthDate = DateTime.Now
+            });
+            modelBuilder.Entity<Prescription>().HasData(new Prescription
+            {
+                IdPrescription = 1,
+                Date = DateTime.Now,
+                DueDate = DateTime.Now.AddDays(2),
+                IdPatient = 1,
+                IdDoctor = 1
+            });
+        }
+    }
+}
