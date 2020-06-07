109bf65b8cddc44c5b8b75f2495a8c9c48194752
Marek Palasek <s17776@pjwstk.edu.pl>
2020-06-07 16:05:41 +0200

    wysłana migracja

---
+++ zajecia6/Migrations/20200607140513_initial.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace zajecia6.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    IdDoctor = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(maxLength: 100, nullable: false),
+                    Email = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.IdDoctor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    IdPatient = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(maxLength: 100, nullable: false),
+                    BirthDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.IdPatient);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prescriptions",
+                columns: table => new
+                {
+                    IdPrescription = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(nullable: false),
+                    DueDate = table.Column<DateTime>(nullable: false),
+                    IdPatient = table.Column<int>(nullable: false),
+                    IdDoctor = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prescriptions", x => x.IdPrescription);
+                    table.ForeignKey(
+                        name: "FK_Prescriptions_Doctors_IdDoctor",
+                        column: x => x.IdDoctor,
+                        principalTable: "Doctors",
+                        principalColumn: "IdDoctor",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Prescriptions_Patients_IdPatient",
+                        column: x => x.IdPatient,
+                        principalTable: "Patients",
+                        principalColumn: "IdPatient",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Doctors",
+                columns: new[] { "IdDoctor", "Email", "FirstName", "LastName" },
+                values: new object[] { 1, "feredekKiepski@gmail.com", "Ferdek", "Kiepski" });
+
+            migrationBuilder.InsertData(
+                table: "Patients",
+                columns: new[] { "IdPatient", "BirthDate", "FirstName", "LastName" },
+                values: new object[] { 1, new DateTime(2020, 6, 7, 16, 5, 12, 670, DateTimeKind.Local).AddTicks(8707), "Marian", "Pazdzioch" });
+
+            migrationBuilder.InsertData(
+                table: "Prescriptions",
+                columns: new[] { "IdPrescription", "Date", "DueDate", "IdDoctor", "IdPatient" },
+                values: new object[] { 1, new DateTime(2020, 6, 7, 16, 5, 12, 675, DateTimeKind.Local).AddTicks(78), new DateTime(2020, 6, 9, 16, 5, 12, 675, DateTimeKind.Local).AddTicks(783), 1, 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prescriptions_IdDoctor",
+                table: "Prescriptions",
+                column: "IdDoctor");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prescriptions_IdPatient",
+                table: "Prescriptions",
+                column: "IdPatient");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prescriptions");
+
+            migrationBuilder.DropTable(
+                name: "Doctors");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+        }
+    }
+}
